70bf770b0ffaf7d91d9bae6255e9bef7c7c04f14
Justin SB <justin@fathomdb.com>
2014-01-31 17:08:09 -0500

    Use Noto Sans UI font

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -375,3 +375,6 @@ end)
 client.add_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.add_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
+
+
+awesome.font = "Noto Sans UI 9"
